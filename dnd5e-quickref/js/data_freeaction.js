739560d98cef807108fa9d63439e15bf1d69e25b
foreveryone1 <afif.hasan96@gmail.com>
2021-02-17 04:27:45 +0100

    Initial commit

---
+++ dnd5e-quickref/js/data_freeaction.js
@@ -0,0 +1,45 @@
+data_freeaction = [
+    {
+        title: "Mark Target",
+        icon: "targeted",
+        subtitle: "When you make a melee attack",
+        description: "Harry opponents in melee",
+        reference: "DMG, pg. 271.",
+        bullets: [
+            "When you make a melee attack, you may mark the target. The target is marked by you until the end of your next turn.",
+            "You make opportunity attacks against your marked targets with advantage.",
+            "You can make opportunity attacks against your marked targets without expending your reaction, provided you are able to take reactions.",
+            "You are limited to one opportunity attack per turn, unless a feature grants you the ability to make multiple opportunity attacks or make opportunity attacks without using your reaction."
+        ]
+    },
+    {
+        title: "Interact with Object",
+        icon: "snatch",
+        subtitle: "One free interaction per turn",
+        description: "",
+        reference: "PHB, pg. 190.",
+        bullets: [
+            "You can interact with one object for free during your turn (such as drawing a weapon or opening a door). If you want to interact with a second object, use the <i>Use Object</i> action."
+        ]
+    },
+    {
+        title: "Drop Item",
+        icon: "drop-weapon",
+        subtitle: "",
+        description: "Drop an item at your feet",
+        reference: "https://twitter.com/JeremyECrawford/status/584435399767883776",
+        bullets: [
+            "You drop an item you are holding to the ground."
+        ]
+    },
+    {
+        title: "Communicate",
+        icon: "screaming",
+        subtitle: "",
+        description: "Communicate briefly",
+        reference: "PHB, pg. 190.",
+        bullets: [
+            "You can communicate however you are able, through brief utterances and gestures, as you take your turn."
+        ]
+    }
+]
